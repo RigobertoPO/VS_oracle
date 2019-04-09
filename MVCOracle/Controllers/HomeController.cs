fa0070e5ad4315c47fa14b40ee89727149b9e267
signsoluciones <reptors58@hotmail.com>
2019-04-09 17:13:55 -0500

    Agregar archivos de proyecto.

---
+++ MVCOracle/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using MVCOracle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCOracle.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();              
+        }
+    }
+}
